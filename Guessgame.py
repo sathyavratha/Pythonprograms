d320fc2fce26ccd625cf3c43f73ad5e5af8c2471
S Sathyavratha <sathyavratha.sara@gmail.com>
2025-04-18 20:42:01 +0530

    Guessgame.py

--- Guessgame.py
+++ Guessgame.py
@@ -1,7 +1,16 @@
+#normal guessing game where user enters a word until its the secret word
+#lets set a limit user should try to guess within
 secret_word="Elephant"
 guess = ""
-
+limit = 0
 while guess != secret_word:
     guess = input("Guess a word! :")
+    limit +=1
+    if limit > 3 :
+        print("Oops! You've used all your chances")
+        print("If you wish to know the secret word type Reveal!")
+        reveal=input()
+        if reveal =='Reveal':
+            print(secret_word)
 
-print("You win")
+print("You win!!!")
